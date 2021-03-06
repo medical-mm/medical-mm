b01daf1e1cda307a4ad74b04f4daa8ab2b1abe4b
medical-mm <80148983+medical-mm@users.noreply.github.com>
2021-03-06 17:51:01 +0630

    Add files via upload

---
+++ template/footer-link.php
@@ -0,0 +1,4 @@
+<script src="vendor/asset/js/jquery.min.js"></script>
+<script src="vendor/asset/bootstrap-5.0.0-beta1-dist/js/bootstrap.min.js"></script>
+<script src="vendor/asset/bootstrap-5.0.0-beta1-dist/js/jquery.dataTables.min.js"></script>
+<script src="vendor/asset/bootstrap-5.0.0-beta1-dist/js/dataTables.bootstrap5.min.js"></script>
